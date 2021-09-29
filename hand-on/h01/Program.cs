c06dc530dbe94942dff4f7baa755206b4baae3da
Bawornsit.C <puksigon@gmail.com>
2021-09-29 20:21:43 +0700

    add h01

---
+++ hand-on/h01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;using h01;
+
+string FizzBuzz(int value) => value % 3 == 0 && value % 5 == 0 ? "FizzBuzz" : value % 3 == 0 ? "Fizz" : value % 5 == 0 ? "Buzz" : "";
+
+FizzBuzz(21).WriteLine();
+FizzBuzz(25).WriteLine();
+FizzBuzz(45).WriteLine();
---
+++ hand-on/h01/StringExtension.cs
@@ -0,0 +1,9 @@
+
+namespace h01;
+public static class StringExtension
+{
+    public static void WriteLine(this string value)
+    {
+        Console.WriteLine(value);
+    }
+}
