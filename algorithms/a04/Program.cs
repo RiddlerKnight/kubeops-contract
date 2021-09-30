3d7feb35bb0f4490d34b6fcaa0b0675071cc2459
Bawornsit.C <puksigon@gmail.com>
2021-09-30 23:22:05 +0700

    Add: algo04

---
+++ algorithms/a04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+static int FiboN(int n)
+{
+    double phi = (Math.Sqrt(5) + 1) / 2;
+    return (int)Math.Round( Math.Pow(phi, n) / Math.Sqrt(5) );
+}
+
+Console.WriteLine(FiboN(2));
+Console.WriteLine(FiboN(9));
+Console.WriteLine(FiboN(8));
+Console.WriteLine(FiboN(5));
